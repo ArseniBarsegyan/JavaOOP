c2daa65cb19d62304305d87acac789cd5e351e02
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-11-05 23:34:51 +0300

    Add task01

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,4 +1,10 @@
+import task01.Cat;
+import task01.Pet;
+
 public class Solution {
     public static void main(String[] args) {
+        Pet pet = new Cat();
+
+        System.out.println(pet.getName());
     }
 }
