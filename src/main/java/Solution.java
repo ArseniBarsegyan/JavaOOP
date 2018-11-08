5d520242ba1f1041b4a43d4c128d6e98a8e86bd5
Nikolai26 <nikolai.miroshnichenko26@gmail.com>
2018-11-08 20:49:21 +0300

    qewe

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,7 +4,7 @@ import task01.Pet;
 public class Solution {
     public static void main(String[] args) {
         Pet pet = new Cat();
-
+int r = 137;
         System.out.println(pet.getName());
     }
 }
