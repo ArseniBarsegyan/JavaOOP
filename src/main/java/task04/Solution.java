fe0f40835054ba94a940d60ff8fd6f0a8cb3613c
Nikolai26 <nikolai.miroshnichenko26@gmail.com>
2018-11-13 19:13:48 +0300

    resolve task04; I'm have a question here

--- src/main/java/task04/Solution.java
+++ src/main/java/task04/Solution.java
@@ -20,9 +20,42 @@ public class Solution {
 
     //TODO - implement your solution
     public static String getObjectType(Object o)
+
     {
-        return null;
+
+
+        if(o instanceof Tiger)
+        {
+            return "Тигр";
+        }
+
+        else if  (o instanceof Lion)
+        {
+            return "Лев";
+        }
+
+        else if  (o instanceof Cat)
+        {
+            return "Кот";
+        }
+
+        else if  (o instanceof Bull)
+        {
+            return "Бык";
+        }
+        else if (o instanceof Cow) {
+            return "Корова";
+        }
+        else if (o instanceof Animal) {
+            return "Животное";
+        }
+        else {
+            return "хз";
+        }
+
     }
+
+
 
     public static class Cat  extends Animal   //<--Классы наследуются!!
     {
