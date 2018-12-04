4572f21341be80f3710a9f35933bee05e22de960
Nikolai26 <nikolai.miroshnichenko26@gmail.com>
2018-12-04 18:37:54 +0300

    1

--- src/main/java/task10/classes/interfaces/CargoCompany.java
+++ src/main/java/task10/classes/interfaces/CargoCompany.java
@@ -3,8 +3,12 @@ package task10.classes.interfaces;
 import java.util.ArrayList;
 
 public abstract class CargoCompany extends Company{
-    private ArrayList <CargoPlane> plane = new  ArrayList <CargoPlane>();
+    private ArrayList <CargoPlane> planes = new  ArrayList <CargoPlane>();
      plane.add();
+
+    public CargoCompany(ArrayList<CargoPlane> planes) {
+
+    }
 
     void addPlane (Plane plane){
         plane.add();
